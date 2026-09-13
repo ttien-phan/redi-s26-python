dc52d73cc54f17ef4b04a211055a71d6b154d6e9
ttien-phan <phan.tttien7@gmail.com>
2026-09-13 15:52:57 +0200

    change directory

---
+++ lesson2/lesson2.py
@@ -0,0 +1,23 @@
+price = int(input("What the price of an item: "))
+quantity = int(input("How many?"))
+
+discount = input("Do you have a discount coupon? ")
+totalPrice = int((price * quantity))
+
+if discount == "yes":
+    print(totalPrice * 0.9)
+else:
+    print(totalPrice)
+
+"""
+quantity = int(input("How many tickets? "))
+status = input("Your membership status? ")
+totalPrice = quantity * 10
+if status == "Gold":
+    print(totalPrice * 0,7)
+elif status == "Silver":
+    totalPrice = totalPrice * 0.85
+    print(totalPrice)
+
+print(totalPrice)
+"""
